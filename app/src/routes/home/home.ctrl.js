7b5a19002a1fb45e2fe366b8db9dcc780ef25256
parksangjong <psj971201@naver.com>
2021-07-22 14:29:08 +0900

    로그인 기능 수정

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,10 @@
 "use strict";
+
+const users = {
+    id:["woorimintIt","나개발","김팀장"],
+    psword:["1234","1234","123456"],
+};
+
 const output = {
    home: (req,res)=>{
     res.render("home/index");
@@ -10,9 +16,25 @@ const output = {
 
 const process = {
     login:(req,res) =>{
-        console.log(req.body);
-    }
-}
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success :false,
+            msg:"로그인에 실패하셨습니다",
+        });
+    },
+};
+
 module.exports = {
     output,
     process,
